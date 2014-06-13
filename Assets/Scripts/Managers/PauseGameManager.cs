1c0c0a5f6da77aa59777895875eed27ebf906d20
Fabricio Lettieri <fabricio.lettieri@scaleoptions.com>
2014-06-13 17:23:29 -0300

    PauseGameManager for pausable components v1.1

--- Assets/Scripts/Managers/PauseGameManager.cs
+++ Assets/Scripts/Managers/PauseGameManager.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PauseGameManager : MonoBehaviour {
 	
-	private IPausable[] arrSceneOnly = null;
-	private IPausable[] arrDurables = null;
+	private List<IPausable> sceneOnly = new List<IPausable>();
+	private List<IPausable> durables = new List<IPausable>();
 		
 	private static PauseGameManager instance = null;
 	
@@ -14,46 +14,29 @@ public class PauseGameManager : MonoBehaviour {
 				// creates a game object with this script component.
 				instance = new GameObject("PauseGameManager").AddComponent<PauseGameManager>();
 				DontDestroyOnLoad(instance);
-				instance.collectDurables();
 			}
             return instance;
         }
     }
 	
-	public void collectDurables() {
-		// collects all pausable components in the scene
-		IPausable[] arr = (IPausable[])FindObjectsOfType(typeof(IPausable));
-		// count durable objects and create the array
-		
-	}
-	
-	public void collectSceneOnly() {
-		arrSceneOnly = null;
-		// collects all pausable components in the scene
-		IPausable[] arr = (IPausable[])FindObjectsOfType(typeof(IPausable));
-		// count durable objects and create the array
-		
+	public void register(IPausable p) {
+		if (p.isSceneOnly())
+			sceneOnly.Add(p);
+		else
+			durables.Add(p);
 	}
 	
 	public void pause() {
-		if (arrSceneOnly != null) {
-			for (int i=0, c=arrSceneOnly.Length; i<c; ++i)
-				arrSceneOnly[i].pause();
-		}
-		if (arrDurables != null) {
-			for (int i=0, c=arrDurables.Length; i<c; ++i)
-				arrDurables[i].pause();
-		}
+		for (int i=0, c=sceneOnly.Count; i<c; ++i)
+			sceneOnly[i].pause();
+		for (int i=0, c=durables.Count; i<c; ++i)
+			durables[i].pause();
 	}
 	
 	public void resume() {
-		if (arrSceneOnly != null) {
-			for (int i=0, c=arrSceneOnly.Length; i<c; ++i)
-				arrSceneOnly[i].resume();
-		}
-		if (arrDurables != null) {
-			for (int i=0, c=arrDurables.Length; i<c; ++i)
-				arrDurables[i].resume();
-		}
+		for (int i=0, c=sceneOnly.Count; i<c; ++i)
+			sceneOnly[i].resume();
+		for (int i=0, c=durables.Count; i<c; ++i)
+			durables[i].resume();
 	}
 }
