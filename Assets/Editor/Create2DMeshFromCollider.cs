d794f6d37b77116d45e842192bf9c90ce2bab486
Fabricio Lettieri <fabricio.lettieri@scaleoptions.com>
2014-07-03 01:16:45 -0300

    Triangulator and 2d Mesh Creator Script from a mesh collider generated previously by 2D ColliderGen

---
+++ Assets/Editor/Create2DMeshFromCollider.cs
@@ -0,0 +1,149 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+/// <summary>
+/// Create a 2D traingulated mesh from a mesh generated from 2D ColliderGen v1.4.
+/// When generating the mesh collider using 2d ColliderGen take note of the property z-thikness.
+/// Use 1 so you don't need to set it in the wizard window of this script. Both values must match.
+/// 
+/// Run from unity editor. This class should be placed in Editor folder.
+/// Created by Fabricio Lettieri on 02.Jul.2014
+/// </summary>
+[RequireComponent(typeof(Triangulator))]
+public class Create2DMeshFromCollider : ScriptableWizard
+{
+	public enum MIRROR_DIRS {
+		NONE,
+		LEFT_TO_RIGHT,
+		RIGHT_TO_LEFT,
+		TOP_TO_BOTTOM,
+		BOTTOM_TO_TOP
+	}
+	
+	// mirroring
+	public MIRROR_DIRS mirrorDir = MIRROR_DIRS.NONE;
+	// z-thickness as in the 2d collider gen
+	public float sourceZThickness = 1;
+	//collider
+	public Mesh _Mesh = null;
+	//Name of Quad Asset
+	public string MeshName = "mesh";
+	//Game Object Name
+	public string GameObjectName = "go_mesh";
+	//Name of asset folder to contain quad asset when created
+	public string AssetFolder = "Assets/Colliders/Generated";
+	
+	
+	[MenuItem("GameObject/Create Other/2D Mesh from 2D ColliderGen")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Create 2D Mesh from a 2D ColliderGen mesh", typeof(Create2DMeshFromCollider));
+    }
+	
+	//Function called when window is created
+	void OnEnable()
+	{
+		//Call selection change to load asset path from selected, if any
+		OnSelectionChange();
+	}
+	
+	//Called 10 times per second
+	void OnInspectorUpdate()
+	{
+	}
+
+	//Function called when window is updated
+	void OnSelectionChange()
+	{
+		//Check user selection in editor - check for folder selection
+		if (Selection.objects != null && Selection.objects.Length == 1)
+		{
+			//Get path from selected asset
+			string assetPath = AssetDatabase.GetAssetPath(Selection.objects[0]);
+			if (assetPath != null && !"".Equals(assetPath))
+				AssetFolder = Path.GetDirectoryName(assetPath);
+		}
+	}
+	
+	//Function to create quad mesh
+	void OnWizardCreate()
+	{
+		if (_Mesh == null) {
+			Debug.LogError("You have to select a mesh");
+			return;
+		}
+		
+		/// 2d ColliderGen v1.4 takes a "z-thickness" property for the mesh generation and locates
+		/// one plane at z-thicknes/2 and the other at -z-thickness/2.
+		/// Also the amount of vertices we set in the "Outline Vertex Count" property is quadruplied.
+		/// Eg: having an outline of 24 verts will generate a mesh collider of 92 verts.
+		/// So we need to extract only frontal vertices (z = z-thickness/2) and avoid duplicates
+		
+		Vector3[] colliderVerts = _Mesh.vertices;
+		HashSet<Vector3> vertsSet = new HashSet<Vector3>(new Vector3Comparer());
+		for (int i=0,c=colliderVerts.Length; i<c; ++i)
+			vertsSet.Add(colliderVerts[i]);
+		
+		// the set should contain colliderVerts/2 verts according to my analysis
+		if (vertsSet.Count != colliderVerts.Length/2) {
+			Debug.LogError("Set of vertices should contain colliderVerts/2 verts. The set has " + 
+				vertsSet.Count + " and colliderVerts/2 = " + colliderVerts.Length/2);
+			return;
+		}
+		
+		// some vertices are duplicated although we only take those frontal ones. So use a set
+		HashSet<Vector2> verts2DSet = new HashSet<Vector2>(new Vector2Comparer());
+		float z = sourceZThickness / 2f;
+		foreach (Vector3 v in vertsSet) {
+			if (v.z == z)
+				verts2DSet.Add((Vector2)v);
+		}
+		Vector2[] verts = new Vector2[verts2DSet.Count];
+		verts2DSet.CopyTo(verts);
+		
+		// triangulate
+		Mesh mesh = Triangulator.CreateMesh3D(verts, 0f); // use 0 for no extruding
+		mesh.name = MeshName;
+		
+		//Create asset in database
+		AssetDatabase.CreateAsset(mesh, AssetDatabase.GenerateUniqueAssetPath(AssetFolder + "/" + MeshName) + ".asset");
+        AssetDatabase.SaveAssets();
+		
+		//Create game object
+		GameObject _2d_mesh = new GameObject(GameObjectName);
+		MeshFilter meshFilter = (MeshFilter)_2d_mesh.AddComponent(typeof(MeshFilter));
+		_2d_mesh.AddComponent(typeof(MeshRenderer));
+		
+		//Assign mesh to mesh filter
+		meshFilter.sharedMesh = mesh;
+		mesh.RecalculateBounds();
+	}
+}
+
+class Vector2Comparer : IEqualityComparer<Vector2>
+{
+  public bool Equals(Vector2 a, Vector2 b)
+  {
+    return a.Equals(b);
+  }
+
+  public int GetHashCode(Vector2 v)
+  {
+    return v.GetHashCode();
+  }
+}
+
+class Vector3Comparer : IEqualityComparer<Vector3>
+{
+  public bool Equals(Vector3 a, Vector3 b)
+  {
+    return a.Equals(b);
+  }
+
+  public int GetHashCode(Vector3 v)
+  {
+    return v.GetHashCode();
+  }
+}
---
+++ Assets/Scripts/Utils/Triangulator.cs
@@ -0,0 +1,203 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+ 
+public class Triangulator {
+	
+	private List<Vector2> m_points = new List<Vector2> ();
+	 
+	public Triangulator (Vector2[] points)
+	{
+		m_points = new List<Vector2> (points);
+	}
+	 
+	public int[] Triangulate ()
+	{
+		List<int> indices = new List<int> ();
+	 
+		int n = m_points.Count;
+		if (n < 3)
+			return indices.ToArray ();
+	 
+		int[] V = new int[n];
+		if (Area () > 0) {
+			for (int v = 0; v < n; v++)
+				V [v] = v;
+		} else {
+			for (int v = 0; v < n; v++)
+				V [v] = (n - 1) - v;
+		}
+	 
+		int nv = n;
+		int count = 2 * nv;
+		for (int m = 0, v = nv - 1; nv > 2;) {
+			if ((count--) <= 0)
+				return indices.ToArray ();
+	 
+			int u = v;
+			if (nv <= u)
+				u = 0;
+			v = u + 1;
+			if (nv <= v)
+				v = 0;
+			int w = v + 1;
+			if (nv <= w)
+				w = 0;
+	 
+			if (Snip (u, v, w, nv, V)) {
+				int a, b, c, s, t;
+				a = V [u];
+				b = V [v];
+				c = V [w];
+				indices.Add (a);
+				indices.Add (b);
+				indices.Add (c);
+				m++;
+				for (s = v, t = v + 1; t < nv; s++, t++)
+					V [s] = V [t];
+				nv--;
+				count = 2 * nv;
+			}
+		}
+	 
+		indices.Reverse ();
+		return indices.ToArray ();
+	}
+	 
+	private float Area ()
+	{
+		int n = m_points.Count;
+		float A = 0.0f;
+		for (int p = n - 1, q = 0; q < n; p = q++) {
+			Vector2 pval = m_points [p];
+			Vector2 qval = m_points [q];
+			A += pval.x * qval.y - qval.x * pval.y;
+		}
+		return (A * 0.5f);
+	}
+	 
+	private bool Snip (int u, int v, int w, int n, int[] V)
+	{
+		int p;
+		Vector2 A = m_points [V [u]];
+		Vector2 B = m_points [V [v]];
+		Vector2 C = m_points [V [w]];
+		if (Mathf.Epsilon > (((B.x - A.x) * (C.y - A.y)) - ((B.y - A.y) * (C.x - A.x))))
+			return false;
+		for (p = 0; p < n; p++) {
+			if ((p == u) || (p == v) || (p == w))
+				continue;
+			Vector2 P = m_points [V [p]];
+			if (InsideTriangle (A, B, C, P))
+				return false;
+		}
+		return true;
+	}
+	 
+	private bool InsideTriangle (Vector2 A, Vector2 B, Vector2 C, Vector2 P)
+	{
+		float ax, ay, bx, by, cx, cy, apx, apy, bpx, bpy, cpx, cpy;
+		float cCROSSap, bCROSScp, aCROSSbp;
+	 
+		ax = C.x - B.x;
+		ay = C.y - B.y;
+		bx = A.x - C.x;
+		by = A.y - C.y;
+		cx = B.x - A.x;
+		cy = B.y - A.y;
+		apx = P.x - A.x;
+		apy = P.y - A.y;
+		bpx = P.x - B.x;
+		bpy = P.y - B.y;
+		cpx = P.x - C.x;
+		cpy = P.y - C.y;
+	 
+		aCROSSbp = ax * bpy - ay * bpx;
+		cCROSSap = cx * apy - cy * apx;
+		bCROSScp = bx * cpy - by * cpx;
+	 
+		return ((aCROSSbp >= 0.0f) && (bCROSScp >= 0.0f) && (cCROSSap >= 0.0f));
+	}
+	 
+	public static Mesh CreateMesh3D (Vector2[] poly, float extrusion)
+	{
+		// convert polygon to triangles
+		Triangulator triangulator = new Triangulator (poly);
+		int[] traingulatedTris = triangulator.Triangulate ();
+		Mesh m = new Mesh ();
+		Vector3[] vertices;
+		if (extrusion == 0f) vertices = new Vector3[poly.Length];
+		else vertices = new Vector3[poly.Length * 2];
+		
+		for (int i = 0; i < poly.Length; i++) {
+			vertices [i].x = poly [i].x;
+			vertices [i].y = poly [i].y;
+			vertices [i].z = -extrusion; // front vertex
+			if (extrusion != 0f) {
+				vertices [i + poly.Length].x = poly [i].x;
+				vertices [i + poly.Length].y = poly [i].y;
+				vertices [i + poly.Length].z = extrusion; // back vertex
+			}
+		}
+	 
+		int[] triangles;
+		if (extrusion == 0f) triangles = new int[traingulatedTris.Length];
+		else triangles = new int[traingulatedTris.Length * 2 + poly.Length * 6];
+		
+		int count_tris = 0;
+		for (int i = 0; i < traingulatedTris.Length; i += 3) {
+			triangles [i] = traingulatedTris [i];
+			triangles [i + 1] = traingulatedTris [i + 1];
+			triangles [i + 2] = traingulatedTris [i + 2];
+		} // front vertices
+	 
+		if (extrusion != 0f) {
+			count_tris += traingulatedTris.Length;
+			for (int i = 0; i < traingulatedTris.Length; i += 3) {
+				triangles [count_tris + i] = traingulatedTris [i + 2] + poly.Length;
+				triangles [count_tris + i + 1] = traingulatedTris [i + 1] + poly.Length;
+				triangles [count_tris + i + 2] = traingulatedTris [i] + poly.Length;
+			} // back vertices
+		}
+		
+		//texture coordinate
+		Vector2[] uvs = new Vector2[vertices.Length];
+	 
+		for (int i = 0; i < uvs.Length; i++) {
+			uvs [i] = new Vector2 (vertices[i].x, vertices[i].z);
+		}
+
+		m.vertices = vertices;
+		m.triangles = triangles;
+		m.uv = uvs;
+		if (extrusion != 0f)
+			m = Triangulator.SideExtrusion (m);
+		m.RecalculateNormals ();
+		m.RecalculateBounds ();
+		m.Optimize ();
+	 
+		return m;
+	}
+	 
+	private static Mesh SideExtrusion (Mesh mesh)
+	{
+		List<int> indices = new List<int> (mesh.triangles);
+		int count = (mesh.vertices.Length / 2);
+		for (int i = 0; i < count; i++) {
+			int i1 = i;
+			int i2 = (i1 + 1) % count;
+			int i3 = i1 + count;
+			int i4 = i2 + count;
+	 
+			indices.Add (i4);
+			indices.Add (i3);
+			indices.Add (i1);
+	 
+			indices.Add (i2);
+			indices.Add (i4);
+			indices.Add (i1);
+		}
+		mesh.triangles = indices.ToArray ();
+		return mesh;
+	}
+}
