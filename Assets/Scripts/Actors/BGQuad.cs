e8dff0dc02d3232e9f8a2b2c93ce7204773631ae
Fabricio Lettieri <fabricio.lettieri@scaleoptions.com>
2014-07-10 23:58:05 -0300

    Modified BGQuad to not draw the background quad if no material has been assigned.

--- Assets/Scripts/Actors/BGQuad.cs
+++ Assets/Scripts/Actors/BGQuad.cs
@@ -6,20 +6,51 @@ public class BGQuad : MonoBehaviour, IScreenLayout {
 	public Texture bgTexture;
 	
 	void Awake () {
+		if (!bgTexture) {
+			gameObject.SetActiveRecursively(false);
+			return;
+		}
 		ScreenLayoutManager.Instance.register(this);
 	}
 	
 	void Start () {
-		fillScreen(); // set the quad to fill the viewport
+		if (!bgTexture)
+			return;
+		// set the quad to fill the viewport
+		fillScreen();
 		renderer.sharedMaterial.mainTexture = bgTexture;
 	}
+
+#if UNITY_EDITOR
+	void OnDestroy () {
+		if (bgTexture)
+			renderer.sharedMaterial.mainTexture = bgTexture;
+	}
+#endif
 	
 	private void fillScreen () {
 		Camera cam = Camera.main;
 		float pos = (cam.nearClipPlane + 0.01f);
 		transform.position = cam.transform.position + cam.transform.forward * pos;
-		float h = Mathf.Tan(cam.fov*Mathf.Deg2Rad*0.5f)*pos*2f;
-		transform.localScale = new Vector3(h*cam.aspect,h,0f);
+		
+		if(!cam.orthographic) {
+			float h = Mathf.Tan(cam.fov * Mathf.Deg2Rad * 0.5f) * pos * 2f;
+			float w = h * cam.aspect;
+			Vector3 theScale = transform.localScale;
+			theScale.x = w;
+			theScale.y = h;
+			theScale.z = 0f;
+			transform.localScale = theScale;
+		}
+		else {
+			float h = cam.orthographicSize * 2f;
+			float w = h / Screen.height * Screen.width;
+			Vector3 theScale = transform.localScale;
+			theScale.x = w;
+			theScale.y = h;
+			theScale.z = 0f;
+			transform.localScale = theScale;
+		}
 	}
 	
 	public void updateSizeAndPosition () {
