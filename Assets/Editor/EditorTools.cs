1d82198a6750e4a8a760ef7e3061a13f433ae5ff
Fabricio Lettieri <fabricio.lettieri@scaleoptions.com>
2014-07-21 21:39:50 -0300

    Moved some editor helper methods to a new helper class named EditorTools

---
+++ Assets/Editor/EditorTools.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+
+static public class EditorTools
+{
+	private delegate void GetWidthAndHeight(TextureImporter importer, ref float width, ref float height);
+    private static GetWidthAndHeight getWidthAndHeightDelegate;
+    
+    public static Vector2 GetOriginalTextureSize(Texture2D texture)
+    {
+        if (texture == null) {
+            Debug.LogError("Texture2D parameter is null");
+			return Vector2.zero;
+		}
+     
+        string path = AssetDatabase.GetAssetPath(texture);
+        if (string.IsNullOrEmpty(path)) {
+			Debug.LogError("Texture2D is not an asset texture.");
+			return Vector2.zero;
+		}
+     
+        var importer = AssetImporter.GetAtPath(path) as TextureImporter;
+        if (importer == null) {
+			Debug.LogError("Failed to get Texture importer for " + path);
+			return Vector2.zero;
+		}
+     
+        return GetOriginalTextureSize(importer);
+    }
+     
+    public static Vector2 GetOriginalTextureSize(TextureImporter importer)
+    {
+        if (getWidthAndHeightDelegate == null) {
+            MethodInfo method = typeof(TextureImporter).GetMethod("GetWidthAndHeight", BindingFlags.NonPublic | BindingFlags.Instance);
+            getWidthAndHeightDelegate = Delegate.CreateDelegate(typeof(GetWidthAndHeight), null, method) as GetWidthAndHeight;
+        }
+     
+        Vector2 vec = new Vector2();
+        getWidthAndHeightDelegate(importer, ref vec.x, ref vec.y);
+     
+        return vec;
+    }
+}
