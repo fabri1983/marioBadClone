49c8e667fa0e8e7fc761d86e9e7756fc743eb85e
Fabricio Lettieri <fabricio.lettieri@scaleoptions.com>
2014-08-27 18:43:17 -0300

    Player: when jumping and user tries to move in opposite direction, it just lessen the velocity

--- Assets/Scripts/Actions/PlayerWalk.cs
+++ Assets/Scripts/Actions/PlayerWalk.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class PlayerWalk : WalkAbs {
 	
-	public float animFPSBoost = 8;
 	public float speedUpFactor = 1.9f;
 	
 	public override void reset () {
@@ -15,15 +14,21 @@ public class PlayerWalk : WalkAbs {
 		if (stop)
 			return;
 		
-		if (Gamepad.isB() || (Input.GetButton("Fire1") && Input.touchCount == 0)) {
+		float velX = base.shape.body.velocity.x;
+		
+		// when jumping and trying to move in opposite direction, just lessen current velocity
+		if (jump.IsJumping() && (velX * velocity) < 0f) {
+			velocity = velX * 0.75f;
+			gain = 1f; // reset gain because of weird issues in velocity
+		}
+		else if (Gamepad.isB() || (Input.GetButton("Fire1") && Input.touchCount == 0)) {
 			gain = speedUpFactor;
-			base._walk(velocity);
-			walkAC.animComp.setFPS(animFPSBoost);
 		}
 		else {
 			gain = 1f;
-			base._walk(velocity);
-			walkAC.animComp.setFPS(walkAC.animFPS);
 		}
+		
+		base._walk(velocity);
+		walkAC.animComp.setFPS(walkAC.animFPS * gain);
 	}
 }
