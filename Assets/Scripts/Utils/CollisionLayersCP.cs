7d80a2f0aedf968ef96c493a0dc88a061ea9f9e1
Fabricio Lettieri <fabricio.lettieri@scaleoptions.com>
2014-08-29 17:13:47 -0300

    Improved the way to gather the number of Unity's Layers. Created an static class which calculates that number once and then cache the value

--- Assets/Scripts/Utils/CollisionLayersCP.cs
+++ Assets/Scripts/Utils/CollisionLayersCP.cs
@@ -8,12 +8,11 @@ using UnityEngine;
 /// </summary>
 public class CollisionLayersCP : MonoBehaviour {
 	
-	private const int LAYERS = 16; // set here what is the max layer slot you defined on Unity's Layers window
-	
 	// add from the Inspector all the layers you want this game object belongs to
 	public int[] otherLayersBelongTo;
 	
-	public void Start(){
+	public void Start ()
+	{		
 		// start from current game object's layer (the one seen in Inspector)
 		uint currentMask = (uint)(1 << gameObject.layer);
 
@@ -23,14 +22,17 @@ public class CollisionLayersCP : MonoBehaviour {
 		
 		// generate a collision mask, as per Chipmunk's layers definition, for game object's current mask
 		uint mask = 0;
-
+		
+		//what is the max valid layer slot you defined on Unity's Layers window
+		int unityLayersCount = CountLayers.countUnitysLayers();
+		
 		/// For every layer this game object belongs: ask what other layer/s it collides with.
 		/// When it collides then set the correct bit mask
-		for (int i=0; i < LAYERS; ++i) {
+		for (int i=0; i < unityLayersCount; ++i) {
 			// if bit i isn't 1 then continue
 			if (((currentMask >> i) & 1) != 1)
 				continue;
-			for (int j=0; j < LAYERS; ++j) {
+			for (int j=0; j < unityLayersCount; ++j) {
 				/// NOTE: - first 8 layers are built-in layers.
 				///       - built-in layers without name aren't ignored whit other built-in layers
 				///         hence you will see them as 1 in the matrix
@@ -42,5 +44,30 @@ public class CollisionLayersCP : MonoBehaviour {
 		// Chipmunk use ~(uint)0 as mask to hit with all shapes
 		
 		GameObjectTools.setLayerForShapes(gameObject, mask);
+	}
+}
+
+/// <summary>
+/// This static class only calculates which is the max valid layer slot you defined on Unity's Layers window.
+/// Once the value is calculated then it's cached.
+/// </summary>
+static class CountLayers {
+	private static int unitys_layers = 0;
+	
+	public static int countUnitysLayers ()
+	{
+		if (unitys_layers != 0)
+			return unitys_layers;
+		
+		// start testing from last layer
+		for (int i=31; i >= 0; --i) {
+			string name = LayerMask.LayerToName(i);
+			if (!"".Equals(name)) {
+				unitys_layers = i + 1;
+				break;
+			}
+		}
+		
+		return unitys_layers;
 	}
 }
