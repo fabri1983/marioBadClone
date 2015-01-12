f76bd1c144a9a1729f16320396f2e171bb430bd4
Fabricio Lettieri <fabri1983@gmail.com>
2015-01-12 18:27:49 -0300

    GUI Screen Layout: allowing offset to be handled as a percentage of current screen dimension.

--- Assets/Scripts/GUI/GUIScreenLayout.cs
+++ Assets/Scripts/GUI/GUIScreenLayout.cs
@@ -8,10 +8,12 @@ public class GUIScreenLayout : MonoBehaviour, IGUIScreenLayout {
 	
 	public bool allowResize = false;
 	public Vector2 offset = Vector2.zero;
+	public bool asPercentile = false;
 	public EnumScreenLayout layout = EnumScreenLayout.BOTTOM_LEFT;
 	
 	private GUICustomElement guiElem; // in case you are using the game object as a GUICustomElement instead as of a GUITexture
-	
+	private Vector2 offsetCalculation; // used to store temporal calculation of offset member as a percentage or pixel-wise of the screen
+
 	void Awake () {
 		// if using with a GUITexture then no GUICustomElement musn't be found
 		guiElem = GetComponent<GUICustomElement>();
@@ -30,13 +32,24 @@ public class GUIScreenLayout : MonoBehaviour, IGUIScreenLayout {
 	}
 	
 	public void updateForGUI () {
+
+		// if using offset as percentage then convert it as pixels
+		if (asPercentile) {
+			offsetCalculation.x = offset.x * Screen.width;
+			offsetCalculation.y = offset.y * Screen.height;
+		}
+		else {
+			offsetCalculation.x = offset.x;
+			offsetCalculation.y = offset.y;
+		}
+
 		// only if we are still using Unity's GUITexture elements
 		if (guiTexture != null) {
 			// first resize
 			if (allowResize)
 				GUIScreenLayoutManager.adjustSize(guiTexture);
 			// then apply position correction
-			GUIScreenLayoutManager.adjustPos(guiTexture, offset, layout);
+			GUIScreenLayoutManager.adjustPos(guiTexture, offsetCalculation, layout);
 		}
 		// we are using GUICustomElement
 		else if (guiElem != null) {
@@ -44,7 +57,7 @@ public class GUIScreenLayout : MonoBehaviour, IGUIScreenLayout {
 			if (allowResize)
 				GUIScreenLayoutManager.adjustSize(guiElem);
 			// then apply position correction
-			GUIScreenLayoutManager.adjustPos(transform, guiElem, offset, layout);
+			GUIScreenLayoutManager.adjustPos(transform, guiElem, offsetCalculation, layout);
 		}
 	}
 
