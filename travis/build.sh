e85e2c4d780e634302acb62f8ce6a71110879cac
pablo.lettieri <pablo.lettieri@etermax.com>
2015-10-28 12:16:54 -0300

    Minor changes in travis scripts

--- travis/build.sh
+++ travis/build.sh
@@ -3,14 +3,6 @@
 project="$APP_NAME"
 unity=/Applications/Unity/Unity.app/Contents/MacOS/Unity
 
-echo "Attempting to build $project for OS X"
-$unity \
-  -batchmode \
-  -nographics \
-  -projectPath $(pwd) \
-  -buildOSXPlayer "$(pwd)/build/osx/$project.app" \
-  -quit
-  
 echo "Attempting to build $project for Windows"
 $unity \
   -batchmode \
@@ -19,6 +11,16 @@ $unity \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/build/windows/$project.exe" \
+  -quit
+
+echo "Attempting to build $project for OS X"
+$unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXPlayer "$(pwd)/build/osx/$project.app" \
   -quit
 
 echo "Attempting to build $project for Linux"
