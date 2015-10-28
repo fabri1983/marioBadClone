9500ad5659ec926c2a9d6d9693da8f1f793bbbcb
Pablo Fabricio Lettieri <fabri1983@gmail.com>
2015-10-28 20:31:59 -0300

    Fixed missing shader value in some materials

--- Assets/Scripts/_AutoGenerated/KLayers.cs
+++ Assets/Scripts/_AutoGenerated/KLayers.cs
@@ -5,7 +5,6 @@ public static class KLayers
 	public const int TRANSPARENT_FX = 1;
 	public const int IGNORE_RAYCAST = 2;
 	public const int WATER = 4;
-	public const int UI = 5;
 	public const int ENEMY = 8;
 	public const int ONLY_WITH_PLAYER = 9;
 	public const int PLAYER = 10;
