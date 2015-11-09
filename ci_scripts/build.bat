9826ae338bb765c13a62952a7fe683cfa2f904a5
pablo.lettieri <pablo.lettieri@etermax.com>
2015-11-09 17:15:59 -0300

    Fixing appveyor.xml compilation errors

--- ci_scripts/build.bat
+++ ci_scripts/build.bat
@@ -18,16 +18,16 @@ set OSX32=-buildOSXPlayer %OSX32_PATH%
 set LINUX32=-buildLinux32Player %LINUX32_PATH%
 
 :: Win32 build
-echo "Running Win Build for: %PROJECT_PATH%"
-echo "%PROGRAMFILES%\Unity\Editor\Unity.exe" %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %WIN32%
-"%ProgramFiles(x86)%\Unity\Editor\Unity.exe" %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %WIN32%
+echo Running Win Build
+echo %UNITY_BIN_DIR%\Unity.exe %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %WIN32%
+%UNITY_BIN_DIR%\Unity.exe %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %WIN32%
 
 :: OSX32 build
-echo "Running OSX Build for: %PROJECT_PATH%"
-echo "%PROGRAMFILES%\Unity\Editor\Unity.exe" %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %OSX32%
-"%ProgramFiles(x86)%\Unity\Editor\Unity.exe" %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %OSX32%
+echo Running OSX Build
+echo %UNITY_BIN_DIR%\Unity.exe %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %OSX32%
+%UNITY_BIN_DIR%\Unity.exe %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %OSX32%
 
 :: Linux build
-::echo "Running Linux Build for: %PROJECT_PATH%"
-::echo "%PROGRAMFILES%\Unity\Editor\Unity.exe" %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %LINUX32%
-::"%ProgramFiles(x86)%\Unity\Editor\Unity.exe" %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %LINUX32%
+::echo Running Linux Build
+::echo %UNITY_BIN_DIR%\Unity.exe" %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %LINUX32%
+::%UNITY_BIN_DIR%\Unity.exe %BUILD_PARAMS% %PROJECT% %PROJECT_PATH% %LINUX32%
