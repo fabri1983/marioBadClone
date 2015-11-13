e14b7f0eb58c7558d577d1e6b13c78fbfc08e466
pablo.lettieri <pablo.lettieri@etermax.com>
2015-11-13 17:42:42 -0300

    Fixing appveyor.yml compilation errors

---
+++ ci_scripts/install_nugget.bat
@@ -0,0 +1,7 @@
+@echo off
+
+NuGet.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"  
+NuGet.exe "Install" "OctopusTools" "-OutputDirectory" "packages" "-ExcludeVersion"  
+NuGet.exe "Install" "Node.js" "-OutputDirectory" "packages" "-ExcludeVersion"  
+NuGet.exe "Install" "Npm.js" "-OutputDirectory" "packages" "-ExcludeVersion"  
+"packages\FAKE\tools\Fake.exe" build.fsx %*
