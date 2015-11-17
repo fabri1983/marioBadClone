3a716bcfa81da8b1aa8d7638cc2c68a470abe4f4
pablo.lettieri <pablo.lettieri@etermax.com>
2015-11-17 17:19:36 -0300

    Added FAKE installation using nugget

--- ci_scripts/build-helpers.fsx
+++ ci_scripts/build-helpers.fsx
@@ -9,7 +9,7 @@ open System.Linq
 open System.Xml
 open System.Diagnostics
 
-let unity_bin = Environment.GetEnvironmentVariable UNITY_BIN
+let unity_bin = Environment.GetEnvironmentVariable "UNITY_BIN"
 
 let possibleUnityPaths = [
     unity_bin
